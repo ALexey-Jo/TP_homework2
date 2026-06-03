1fc5d031c8711ed632e6a8869962a66430ea35d2
Alexx-J <noobpenek100@gmail.com>
2026-06-03 23:30:52 +0000

    добавил ветку main, куда положил проект

---
+++ main/Recipe.py
@@ -0,0 +1,31 @@
+class Recipe:
+    def __init__(self, title: str, ingredients: list):
+        self.title = title 
+        self.ingredients = ingredients
+    
+    def add_ingridients(self, ingredient: Ingredient):
+        for i in self.ingredients:  
+            if i == ingredient:
+                i.quantity += ingredient.quantity
+                return 
+        self.ingredients.append(ingredient)
+
+    @staticmethod
+    def is_valid_ratio(ratio):
+        if isinstance(ratio, bool):
+            return False
+        return isinstance(ratio, (int, float)) and ratio > 0
+    
+    def scale(self, ratio: float):
+        new_ingr = [Ingredient(i.name, i.quantity * ratio, i.unit) for i in self.ingredients]
+        return Recipe(self.title, new_ingr)
+    
+    def __len__(self):
+        return len(self.ingredients)
+    
+    def __str__(self):
+        l = [f"Рецепт: {self.title}"]
+        for i in self.ingredients:
+            l.append(f" - {i}")
+        return "\n".join(l)
+    
