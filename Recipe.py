28fea212af431924991ba7c23ee34b82a4f8027e
Alexx-J <noobpenek100@gmail.com>
2026-06-03 14:44:00 +0000

    по идее дописал рецепты

--- Recipe.py
+++ Recipe.py
@@ -14,5 +14,18 @@ class Recipe:
     def is_valid_ratio(ratio):
         if isinstance(ratio, bool):
             return False
-        return isinstance(ratio, )
-        
+        return isinstance(ratio, (int, float)) and ratio > 0
+    
+    def scale(self, ratio: float):
+        new_ingr = [Ingredient(i.name, i.quantity * ratio, i.unit) for i in self.ingredients]
+        return Recipe(self.titlem, new_ingr)
+    
+    def __len__(self):
+        return len(self.ingredients)
+    
+    def __str__(self):
+        l = [f"Рецепт: {self.title}"]
+        for i in self.ingredients:
+            l.append(f" - {i}")
+        return "\n".join(l)
+    
